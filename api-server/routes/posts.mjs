7f73623a73f8afe5e0ad797488649444d1e3dc77
Mansura-0-0 <s00265100@atu.ie>
2026-04-10 13:26:03 +0100

    bootstrap insatlled

--- api-server/routes/posts.mjs
+++ api-server/routes/posts.mjs
@@ -4,48 +4,36 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
-// Get a list of 50 posts
+// Get all perfumes
 router.get("/", async (req, res) => {
-  let collection = await db.collection("perfume");
+  let collection =  db.collection("perfume");
   let results = await collection.find({})
-    .limit(50)
     .toArray();
-  res.send(results).status(200);
+  res.json(results);
 });
 
-// Get a single post
+// Get a single perfume
 router.get("/:id", async (req, res) => {
-  let id=req.params.id
-  console.log(id)
-  let collection = await db.collection("perfume");
-  let query = {_id: ObjectId(id)};
-  let result = await collection.findOne(query);
-  if (!result) 
-    res.send("Not found").status(404);
-  else 
-    res.send(result).status(200);
+  let collection =  db.collection("perfume");
+  let result = await collection.findOne({ 
+    _id: new ObjectId(req.params.id) });
+    res.json(result);
 });
 
-// Add a new document to the collection
+// Add a new perfum to the collection
 router.post("/", async (req, res) => {
-  let collection = await db.collection("perfume");
-  let newDocument = req.body;
-  newDocument.date = new Date();
-  let result = await collection.insertOne(newDocument);
-  res.send(result).status(204);
+  let collection =  db.collection("perfume");
+  let result = await collection.insertOne(req.body);
+  res.json(result);
 });
 
 // Delete an entry
 router.delete("/:id", async (req, res) => {
-  let id=req.params.id
-  console.log("del : " + id)
-  const query = { _id: ObjectId(id) };
+  let collection =  db.collection("perfume");
+  let result = await collection.deleteOne({ 
+    _id: new ObjectId(req.params.id) });
+    res.json(result);
 
-  const collection = db.collection("perfume");
-  console.log(query);
-  let result = await collection.deleteOne(query);
-
-  res.send(result).status(200);
 
 });
 
